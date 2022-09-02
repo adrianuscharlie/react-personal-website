37ba34cfdfb7fb79f091241ca895907f15cf3107
adrianuscharlie <adrianus.charlie02@gmail.com>
2022-09-02 13:21:15 +0700

    Fix hamburger button bug

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,6 +1,14 @@
 import React from "react";
 
 export default function Navbar(){
+  function handleClick(){
+    const hamburger=document.querySelector('#hamburger');
+    const navMenu=document.querySelector('#nav-menu');
+    hamburger.addEventListener('click',function(){
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+})
+  }
   return (
     <header className="bg-transparent absolute top-0 left-0 w-full flex items-center z-10">
         <div className="container">
@@ -9,7 +17,7 @@ export default function Navbar(){
                 <a href="#home" className="font-bold text-primary  text-lg block py-6">Adrianus Charlie</a>
               </div>
               <div className="flex items-center px-4">
-                <button id="hamburger" name="hamburger" type="button" className=" hamburger block absolute right-4 lg:hidden">
+                <button id="hamburger" name="hamburger" type="button" className=" hamburger block absolute right-4 lg:hidden" onClick={handleClick}>
                   <span className="hamburger-line transition duration-300 ease-in-out origin-top-left"></span>
                   <span className="hamburger-line transition duration-300 ease-in-out"></span>
                   <span className="hamburger-line transition duration-300 ease-in-out origin-bottom-left"></span>
